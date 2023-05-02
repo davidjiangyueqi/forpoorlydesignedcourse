a0a7ab25f83b2e41cf948283ba44d70e8d9ee1fe
davidjiangyueqi <55066775+davidjiangyueqi@users.noreply.github.com>
2023-05-02 00:46:28 -0500

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def serve():
+    if request.method == 'POST':
+        subprocess.Popen(["python3", "stress_cpu.py"]) 
+        hostname = socket.gethostname()
+        return f"POST@EC2 {socket.gethostbyname(hostname)}"
+    else:
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000)
